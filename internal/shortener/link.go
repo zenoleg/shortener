37280fdf332be2e9d156c69f7beafe7253abd2f8
Zenkovich <zenkovicholeg@gmail.com>
2024-06-02 19:18:13 +0300

    link value-objects init

---
+++ internal/shortener/link.go
@@ -0,0 +1,68 @@
+package shortener
+
+import (
+	"net/url"
+	"strings"
+
+	"emperror.dev/errors"
+	"github.com/jxskiss/base62"
+)
+
+type (
+	originalURL struct {
+		original string
+	}
+
+	shortURL struct {
+		encodedValue string
+	}
+
+	link struct {
+		original originalURL
+		short    shortURL
+	}
+)
+
+func newOriginalURL(original string) (originalURL, error) {
+	original = strings.TrimSpace(original)
+
+	if original == "" {
+		return originalURL{}, errors.New("original url is empty")
+	}
+
+	_, err := url.ParseRequestURI(original)
+
+	if err != nil {
+		return originalURL{}, err
+	}
+
+	return originalURL{original: original}, nil
+}
+
+func newShortURL(originalURL originalURL) shortURL {
+	encoded := base62.Encode([]byte(originalURL.String()))
+
+	return shortURL{
+		encodedValue: string(encoded),
+	}
+}
+
+func newLink(original string) (link, error) {
+	originalValue, err := newOriginalURL(original)
+	if err != nil {
+		return link{}, err
+	}
+
+	return link{
+		original: originalValue,
+		short:    newShortURL(originalValue),
+	}, nil
+}
+
+func (u originalURL) String() string {
+	return u.original
+}
+
+func (u shortURL) String() string {
+	return u.encodedValue
+}
---
+++ internal/shortener/link_test.go
@@ -0,0 +1,71 @@
+package shortener
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOriginalURL(t *testing.T) {
+	t.Parallel()
+
+	t.Run("When passed URL value is empty, then return an error", func(t *testing.T) {
+		url, err := newOriginalURL(" ")
+
+		assert.Error(t, err)
+		assert.Equal(t, originalURL{}, url)
+	})
+
+	t.Run("When passed URL value is invalid, then return an error", func(t *testing.T) {
+		url, err := newOriginalURL("invalid-url")
+
+		assert.Error(t, err)
+		assert.Equal(t, originalURL{}, url)
+	})
+
+	t.Run("When passed URL contains spaces, then trim them", func(t *testing.T) {
+		url, err := newOriginalURL(" https://google.com ")
+
+		assert.NoError(t, err)
+		assert.Equal(t, "https://google.com", url.String())
+	})
+
+	t.Run("When passed URL value is valid, then return an error", func(t *testing.T) {
+		original := "https://google.com/?key=value"
+		url, err := newOriginalURL(original)
+
+		assert.NoError(t, err)
+		assert.Equal(t, original, url.String())
+	})
+}
+
+func TestShortURL(t *testing.T) {
+	t.Run("When passed original URL is valid, then return a short URL", func(t *testing.T) {
+		original, err := newOriginalURL("https://google.com")
+		short := newShortURL(original)
+
+		assert.NoError(t, err)
+		assert.Equal(t, "t92YuUGbn92bn9yL6MHc0RHa", short.String())
+	})
+}
+
+func TestLink(t *testing.T) {
+	t.Parallel()
+
+	t.Run("When passed original URL is invalid, then return an error", func(t *testing.T) {
+		lnk, err := newLink("invalid-url")
+
+		assert.Error(t, err)
+		assert.Equal(t, link{}, lnk)
+	})
+
+	t.Run("When passed original URL is valid, then return a link", func(t *testing.T) {
+		lnk, err := newLink("https://google.com")
+
+		assert.NoError(t, err)
+		assert.Equal(t, link{
+			original: originalURL{original: "https://google.com"},
+			short:    shortURL{encodedValue: "t92YuUGbn92bn9yL6MHc0RHa"},
+		}, lnk)
+	})
+}
