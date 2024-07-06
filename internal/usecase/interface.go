3f2b08abdec1f40067b76356dbfeb43e64b0de86
Zenkovich <zenkovicholeg@gmail.com>
2024-07-06 23:29:13 +0300

    add mocs

--- internal/usecase/interface.go
+++ internal/usecase/interface.go
@@ -2,16 +2,17 @@ package usecase
 
 import "github.com/zenoleg/shortener/internal/domain"
 
-type (
-	WriteOnlyStorage interface {
-		Store(shortenURL domain.ShortenURL) error
-	}
+//go:generate go run github.com/vektra/mockery/v2@v2.43.2 --name=WriteOnlyStorage
+type WriteOnlyStorage interface {
+	Store(shortenURL domain.ShortenURL) error
+}
 
-	ReadOnlyStorage interface {
-		GetOriginalURL(id domain.ID) (domain.URL, error)
-	}
+//go:generate go run github.com/vektra/mockery/v2@v2.43.2 --name=ReadOnlyStorage
+type ReadOnlyStorage interface {
+	GetOriginalURL(id domain.ID) (domain.URL, error)
+}
 
-	IDGenerator interface {
-		Generate(originalURL domain.URL) domain.ID
-	}
-)
+//go:generate go run github.com/vektra/mockery/v2@v2.43.2 --name=IDGenerator
+type IDGenerator interface {
+	Generate(originalURL domain.URL) domain.ID
+}
